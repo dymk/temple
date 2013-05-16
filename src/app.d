7229ad67e23a3648ab3b996035f75a33f028d275
Dylan Knutson <tcdknutson@gmail.com>
2013-05-16 12:34:19 -0700

    app.d added for unittest harness; run dub to build + test

---
+++ src/app.d
@@ -0,0 +1,8 @@
+import templ.templ;
+
+version(unittest) {
+	pragma(msg, "Compiling templ with tests...");
+} else {
+	static assert(false, "Please build with -unittest when compiling directly");
+}
+void main() {}
