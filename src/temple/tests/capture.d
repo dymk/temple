d6fb7f9a2ba605354149d0f2ff6b10bd1e63787b
Dylan Knutson <tcdknutson@gmail.com>
2014-01-02 16:34:59 -1000

    move tests into temple.tests

---
+++ src/temple/tests/capture.d
@@ -0,0 +1,132 @@
+module temple.tests.capture;
+
+import
+	temple.tests.common,
+	temple.temple,
+	temple.output_stream;
+
+unittest
+{
+	// test captures
+	alias render = Temple!q{
+		<% auto a = capture(() { %>
+			This is captured in A
+		<% }); %>
+		<% auto b = capture(() { %>
+			This is captured in B
+		<% }); %>
+
+		B said: "<%= b %>"
+		A said: "<%= a %>"
+	};
+
+	auto accum = new AppenderOutputStream;
+	render(accum);
+
+	assert(isSameRender(accum.data, `
+		B said: "This is captured in B"
+		A said: "This is captured in A"
+	`));
+}
+
+unittest
+{
+	// Nested captures
+	alias render = Temple!q{
+		<% auto outer = capture(() { %>
+			Outer, first
+			<% auto inner = capture(() { %>
+				Inner, first
+			<% }); %>
+			Outer, second
+
+			<%= inner %>
+		<% }); %>
+
+		<%= outer %>
+	};
+
+	auto accum = new AppenderOutputStream;
+	render(accum);
+
+	assert(isSameRender(accum.data, `
+		Outer, first
+		Outer, second
+			Inner, first
+	`));
+}
+
+unittest
+{
+	alias render = TempleFile!"test8_building_helpers.emd";
+	assert(isSameRender(templeToString(&render), readText("test/test8_building_helpers.emd.txt")));
+}
+
+unittest
+{
+	alias render = Temple!q{
+		<%= capture(() { %>
+			directly printed
+
+			<% auto a = capture(() { %>
+				a, captured
+			<% }); %>
+			<% auto b = capture(() { %>
+				b, captured
+			<% }); %>
+
+			<%= a %>
+			<%= capture(() { %>
+				directly printed from a nested capture
+			<% }); %>
+			<%= b %>
+
+		<% }); %>
+	};
+
+	auto accum = new AppenderOutputStream;
+	render(accum);
+
+	assert(isSameRender(accum.data, `
+		directly printed
+			a, captured
+			directly printed from a nested capture
+			b, captured`));
+}
+
+/**
+ * Test CTFE compatibility
+ */
+unittest
+{
+	alias render = Temple!q{ <%= "foo" %> };
+	const result = templeToString(&render);
+	static assert(isSameRender(result, "foo"));
+}
+
+unittest
+{
+	alias render = Temple!q{
+		<% if(true) { %>
+			Bort
+		<% } else { %>
+			No bort!
+		<% } %>
+
+		<% auto a = capture(() { %>
+			inside a capture block
+		<% }); %>
+
+		Before capture
+		<%= a %>
+		After capture
+	};
+
+	const result = templeToString(&render);
+	static assert(isSameRender(result, `
+		Bort
+		Before capture
+		inside a capture block
+		After capture
+	`));
+}
---
+++ src/temple/tests/filter_policy.d
@@ -0,0 +1,113 @@
+module temple.tests.filter_policy;
+
+import
+	temple.tests.common,
+	temple.temple,
+	temple.output_stream;
+
+unittest
+{
+	static struct Filter
+	{
+		static string templeFilter(string raw_str)
+		{
+			return "!" ~ raw_str ~ "!";
+		}
+	}
+
+	alias render1 = Temple!(Filter, `<%= "foo" %> bar`);
+	assert(templeToString(&render1) == "!foo! bar");
+
+	alias render2 = TempleFile!(Filter, "test9_filter_policy.emd");
+	assert(isSameRender(templeToString(&render2), `!foo! bar`));
+}
+
+unittest
+{
+	static struct Filter
+	{
+		static string templeFilter(string raw_str)
+		{
+			return "!" ~ raw_str ~ "!";
+		}
+	}
+
+	alias layout = TempleLayoutFile!(Filter, "test10_fp_layout.emd");
+	alias partial = TempleFile!(Filter, "test10_fp_partial.emd");
+
+	//writeln(templeToString(&layout, &partial));
+	assert(isSameRender(templeToString(&layout, &partial), readText("test/test10_fp.emd.txt")));
+}
+
+unittest
+{
+	static struct FilterPolicy
+	{
+		static struct TaintedString
+		{
+			string value;
+			bool clean = false;
+		}
+
+		static string templeFilter(TaintedString ts)
+		{
+			if(ts.clean)
+			{
+				return ts.value;
+			}
+			else
+			{
+				return "!" ~ ts.value ~ "!";
+			}
+		}
+
+		static string templeFilter(string str)
+		{
+			return templeFilter(TaintedString(str));
+		}
+
+		static TaintedString safe(string str)
+		{
+			return TaintedString(str, true);
+		}
+	}
+
+	alias render1 = Temple!(FilterPolicy, q{
+		foo (filtered):   <%= "mark me" %>
+		foo (unfiltered): <%= safe("don't mark me") %>
+	});
+
+	assert(isSameRender(templeToString(&render1), `
+		foo (filtered):   !mark me!
+		foo (unfiltered): don't mark me
+	`));
+
+	alias render2 = Temple!(FilterPolicy, q{
+		<%
+		auto helper1(void delegate() block)
+		{
+			return "a " ~ capture(block) ~ " b";
+		}
+		%>
+
+		<%= capture(() { %>
+			foo1
+			<%= "foo2" %>
+		<% }); %>
+
+		<%= helper1(() { %>
+			<%= "foo3" %>
+		<% }); %>
+
+		<%= helper1(() { %>
+			<%= safe("foo4") %>
+		<% }); %>
+	});
+
+	assert(isSameRender(templeToString(&render2), `
+		foo1
+		!foo2!
+		a !foo3! b
+		a foo4 b
+	`));
+}
