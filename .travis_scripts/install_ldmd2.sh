b08af57ac2d41365ab7014247e5785ad3d742b0b
Dylan Knutson <tcdknutson@gmail.com>
2014-01-02 22:02:33 -1000

    bump LDC ~merge-2.064 to later version

--- .travis_scripts/install_ldmd2.sh
+++ .travis_scripts/install_ldmd2.sh
@@ -3,7 +3,7 @@
 sudo apt-get install cmake llvm-3.2 libconfig++-dev
 git clone https://github.com/ldc-developers/ldc.git ldc
 cd ldc
-git checkout dd3cc5f6
+git checkout a24b8b69
 git submodule update --init
 mkdir build && cd build
 cmake ..
